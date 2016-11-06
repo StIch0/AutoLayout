8c89658be582bac4176ac87ca80bfc176dce7f8f
StIch0 <loluich@gmail.com>
2016-11-06 16:36:36 +0900

    Assets
    0,1 Версия.
    Начало.
    Создание почвы и фундамента + стены

---
+++ Assets/Script/CreateWalls.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateWalls : MonoBehaviour {
+   public GameObject plane;
+  Object clone;
+    void Start()
+    {
+
+        Create();
+    }
+    void Create()
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                clone =  Instantiate(plane, new Vector3(i * 1.0f, 0, j*1.0f), Quaternion.identity) ;
+                clone.name = "Plane " + i+ " " +j;
+            }
+        }
+       
+    }
+
+}
---
+++ Assets/Script/Test.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+    public  GameObject foundation;
+    //void FixedUpdate()
+    //{
+    //    Rotation();
+    //}
+    //public void Rotation()
+    //{
+    //    if (Input.GetKeyUp(KeyCode.Alpha8))
+    //    {
+    //        print("90");
+    //        foundation.transform.rotation = new Quaternion(0.0f, 90.0f, 0.0f,0.0f);
+    //    }
+    //}
+    public void OnMouseDown()
+    {
+     Object f=  Instantiate(foundation, new Vector3(GetComponent<Renderer>().transform.position.x, 0, GetComponent<Renderer>().transform.position.z), Quaternion.identity);
+        f.name = "foundation " + GetComponent<Renderer>().transform.position.x+ " " +GetComponent<Renderer>().transform.position.z;
+    }
+
+    public void OnMouseEnter()
+    {
+
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+    public void OnMouseExit()
+    {
+
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
